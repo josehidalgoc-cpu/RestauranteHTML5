2d10d18b462ac3d1f545aa4ee136893e054bef40
josehidalgoc-cpu <josehidalgoc@uees.edu.ec>
2026-09-03 19:37:56 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const contenedor = document.querySelector("#menu");
+
+menu.forEach(plato => {
+    contenedor.innerHTML += `<div class="menu-item">${plato.nombre} - $${plato.precio}</div>`;
+});
---
+++ js/data.js
@@ -0,0 +1,5 @@
+const menu = [
+    { nombre: "Ceviche", precio: 0 },
+    { nombre: "Pollo horneado", precio: 10 },
+];
+
